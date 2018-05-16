369a409fc1dac4c4a0b611e4e4f059659f9e616a
Dinesh Sharma <39302151+MeetDineshSharma@users.noreply.github.com>
2018-05-16 15:03:25 +0530

    Update IfApp.java

--- IfApp.java
+++ IfApp.java
@@ -1,8 +1,9 @@
+// As we learned in previous file "HelloJava", this file has a class with the name IfApp
 public class IfApp{
- public static void main(String[] args){
-  int x = 2;
-  if (x < 4){
-   System.out.println("This is less than 4");
+ public static void main(String[] args){ // this is our main method
+  int x = 2; // we declare an integer type variable "x" and gave it the value "2"
+  if (x < 4){ // here we put the condition that if the value of x is less than 4, keep printing "this is less than four"
+   System.out.println("This is less than 4"); // this is the output we want when the condition is true
   }
  }
 }
